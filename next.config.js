186c49291f73b5bd2ff77f1ed541c2465337a5a9
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-08-03 16:22:17 +0900

    変更

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 module.exports = {
+  output: "export",
   // basePath: process.env.GITHUB_ACTIONS && "/tools/",
   // trailingSlash: true,
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'chakkun1121.github.io',
-        port: '',
-        pathname: '/tools/**',
+        protocol: "https",
+        hostname: "chakkun1121.github.io",
+        port: "",
+        pathname: "/tools/**",
       },
     ],
-  }
+  },
 };
